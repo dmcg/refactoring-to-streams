2eaef18135af000ce3ffbb0fd9bb79057a1f8453
Duncan McGregor <duncan@oneeyedmen.com>
2015-05-31 15:26:55 +0100

    Parallelise Summing

--- src/test/java/org/spaconference/rts/ExB_Summing.java
+++ src/test/java/org/spaconference/rts/ExB_Summing.java
@@ -5,7 +5,7 @@ import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
-import java.util.function.IntFunction;
+import java.util.function.LongFunction;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,30 +15,38 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ExB_Summing {
 
     @DataPoint
-    public static IntFunction<Integer> FOR_LOOP = limit -> {
-        int result = 0;
-        for (int i = 0; i < limit; i++) {
+    public static Long2Long FOR_LOOP = limit -> {
+        long result = 0;
+        for (long i = 0; i < limit; i++) {
             result += i;
         }
         return result;
     };
 
     @DataPoint
-    public static IntFunction<Integer> STREAM = null;
+    public static Long2Long STREAM = limit -> { return 0L; };
 
 
     @Theory
-    public void is_0_for_limit_0(IntFunction<Integer> sum) {
-        assertThat(sum.apply(0), equalTo(0));
+    public void is_0_for_limit_0(Long2Long sum) {
+        assertThat(sum.apply(0), equalTo(0L));
     }
 
     @Theory
-    public void is_3_for_limit_3(IntFunction<Integer> sum) {
-        assertThat(sum.apply(3), equalTo(3));
+    public void is_3_for_limit_3(Long2Long sum) {
+        assertThat(sum.apply(3), equalTo(3L));
     }
 
     @Theory
-    public void is_6_for_limit_4(IntFunction<Integer> sum) {
-        assertThat(sum.apply(4), equalTo(6));
+    public void is_6_for_limit_4(Long2Long sum) {
+        assertThat(sum.apply(4), equalTo(6L));
     }
+
+    @Theory
+    public void is_big_for_MAX_VALUE(Long2Long sum) {
+        assertThat(sum.apply(Integer.MAX_VALUE), equalTo(2305843005992468481L));
+    }
+
+    static interface Long2Long extends LongFunction<Long> {}
+
 }
