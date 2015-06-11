1283b52fb58ba79abc35ce5310592cdcf9123f8e
Duncan McGregor <duncan@oneeyedmen.com>
2015-06-11 10:53:27 +0100

    Add a Collecting exercise, plus worked steps

---
+++ src/test/java/org/spaconference/rts/ExB1_Collecting.java
@@ -0,0 +1,33 @@
+package org.spaconference.rts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.spaconference.rts.runner.ExampleRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.spaconference.rts.runner.ExampleRunner.Way;
+
+
+@RunWith(ExampleRunner.class)
+public class ExB1_Collecting {
+
+    @Way
+    public static List<String> oldWay(Iterable<String> things) {
+        List<String> result = new ArrayList<>();
+        for (String thing : things) {
+            result.add(thing);
+        }
+        return result;
+    }
+
+    @Test
+    public void test(Function<Iterable<String>, List<String>> f) {
+        assertThat(f.apply(asList("one", "two", "three")), equalTo(asList("one", "two", "three")));
+    }
+}
